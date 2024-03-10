7b3b45f58cc810d149eb4d6d625375869ce54d05
Pratik Dhimal <s.dhimal006@gmail.com>
2024-03-10 10:06:59 +0545

    IN PROGRESS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const page2__cards = document.querySelector(".page2__cards");
 const dummyData = [
   {
     url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "Hello",
+    title: "hellow rold",
     desc: "This is a dummy description",
   },
   {
@@ -12,7 +12,7 @@ const dummyData = [
   },
   {
     url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "Hello",
+    title: "Image",
     desc: "This is a dummy description",
   },
   {
@@ -22,19 +22,18 @@ const dummyData = [
   },
 ];
 
+dummyData.map((item) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-dummyData.map(item => {
-  const card = document.createElement('div');
-  card.classList.add('card');
-
-  const image = document.createElement('img');
+  const image = document.createElement("img");
   image.src = item.url;
   image.alt = item.title;
 
-  const title = document.createElement('h2');
+  const title = document.createElement("h2");
   title.textContent = item.title;
 
-  const desc = document.createElement('p');
+  const desc = document.createElement("p");
   desc.textContent = item.desc;
 
   card.appendChild(image);
@@ -42,4 +41,62 @@ dummyData.map(item => {
   card.appendChild(desc);
 
   page2__cards.appendChild(card);
+});
+
+const cardData = [
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 1 ",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 2",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 3",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 1",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 2 ",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    description: "This is description part 3 ",
+  },
+];
+
+const container = document.querySelector(".page3__main");
+
+cardData.forEach((cardInfo) => {
+  const card = document.createElement("div");
+  card.classList.add("page3__card");
+
+  const imgHolder = document.createElement("div");
+  imgHolder.classList.add("page3__imgHolder");
+
+  const img = document.createElement("img");
+  img.classList.add("page3__img");
+  img.src = cardInfo.imageUrl;
+  img.alt = "Card Image";
+
+  const imgDesc = document.createElement("div");
+  imgDesc.classList.add("page3__imgDesc");
+  imgDesc.innerHTML = `<h1>${cardInfo.description}</h1>`;
+
+  imgHolder.appendChild(img);
+  card.appendChild(imgHolder);
+  card.appendChild(imgDesc);
+
+  container.appendChild(card);
 });

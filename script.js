23d2a7ac3574bb0b88e62a1f7a0dc87ab9b4d8b7
Pratik Dhimal <s.dhimal006@gmail.com>
2024-03-11 21:35:51 +0545

    ready for deployment

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const page2__cards = document.querySelector(".page2__cards");
 const dummyData = [
   {
-    url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "hellow rold",
-    desc: "This is a dummy description",
+    url: "https://images.pexels.com/photos/159775/library-la-trobe-study-students-159775.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Learning Zone",
+    desc: "We have dedicated studying spaces ",
   },
   {
-    url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "Hello",
-    desc: "This is a dummy description",
+    url: "https://images.pexels.com/photos/2422294/pexels-photo-2422294.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Collaboration",
+    desc: "We collaborate with students and teachers",
   },
   {
-    url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "Image",
-    desc: "This is a dummy description",
+    url: "https://images.pexels.com/photos/518244/pexels-photo-518244.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Student Service",
+    desc: "Our SSD is the best",
   },
   {
-    url: "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    title: "Hello",
-    desc: "This is a dummy description",
+    url: "https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Free Internet",
+    desc: "We offer free internet throughout the campus",
   },
 ];
 
@@ -46,33 +46,33 @@ dummyData.map((item) => {
 const cardData = [
   {
     imageUrl:
-      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 1 ",
+      "https://images.pexels.com/photos/7092339/pexels-photo-7092339.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "Our classerooms are well equiped",
   },
   {
     imageUrl:
-      "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 2",
+      "https://images.pexels.com/photos/6334870/pexels-photo-6334870.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "We have well equiped library",
   },
   {
     imageUrl:
-      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 3",
+      "https://images.pexels.com/photos/6325958/pexels-photo-6325958.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "Our tutots are expert in their fields",
   },
   {
     imageUrl:
-      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 1",
+      "https://images.pexels.com/photos/8199159/pexels-photo-8199159.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "We give opportunities to students",
   },
   {
     imageUrl:
-      "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 2 ",
+      "https://images.pexels.com/photos/6326382/pexels-photo-6326382.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "We give online resources as well",
   },
   {
     imageUrl:
-      "https://images.pexels.com/photos/20522440/pexels-photo-20522440/free-photo-of-a-photo-of-snow-covered-mountains.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    description: "This is description part 3 ",
+      "https://images.pexels.com/photos/7396389/pexels-photo-7396389.jpeg?auto=compress&cs=tinysrgb&w=800",
+    description: "We care our students",
   },
 ];
 
@@ -100,3 +100,30 @@ cardData.forEach((cardInfo) => {
 
   container.appendChild(card);
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+}
